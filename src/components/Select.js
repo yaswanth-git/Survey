06307a20dc793dda4a55586a9890767aa6192aee
Yaswanth <yaswanth.offl@gmail.com>
2021-05-17 22:01:20 +0530

    Update Select.js

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import Option from "./Option";
-
+import "../styles.css";
 const Select = (props) => {
     const [init, setInit] = useState("");
     if (init === "") {
         return (
-            <div>
-                <select onChange={(e) => {
+            <div className="divs">
+                <select className="select" onChange={(e) => {
                     setInit(e.target.value);
                 }}>
-                    <option value="">Selct the type of question</option>
+                    <option value="">Select the type of question</option>
                     <option value="single">Single select</option>
                     <option value="multi">Multi-select</option>
                 </select>
@@ -18,14 +18,14 @@ const Select = (props) => {
     }
     else if (init === "single") {
         return (
-            <div className="App">
+            <div className="divs">
                 <Option limit="2" dom={props.dom} setDom={props.setDom} />
             </div>
         )
     }
     else {
         return (
-            <div className="App">
+            <div className="divs">
             <Option limit="4" dom={props.dom} setDom={props.setDom} />
             </div>
         )
